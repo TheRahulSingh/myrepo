bb78f7de7791ca83ebe7790e2d08d42a11c7b2b2
TheRahulSingh <rahul1851@gmail.com>
2024-07-20 17:05:14 +0530

    Updated review comments

--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,26 @@
-operator = input("Enter an operator ( + - * /): ")
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
 
-if operator == "+":
-    result = num1 + num2
+def takeinput():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    calculate(num1, num2)
+
+
+def calculate(num1, num2):
+    global result
+    operator = input("Enter an operator ( + - * /): ")
+    if operator == "+":
+        result = num1 + num2
+    elif operator == "-":
+        result = num1 - num2
+    elif operator == "*":
+        result = num1 * num2
+    elif operator == "/":
+        result = num1 / num2
+    printfunction(result)
+
+
+def printfunction(result):
     print(round(result, 4))
-elif operator == "-":
-    result = num1 - num2
-    print(round(result, 4))
-elif operator == "*":
-    result = num1 * num2
-    print(round(result, 4))
-elif operator == "/":
-    result = num1 / num2
-    print(round(result, 4))
+
+
+takeinput()
